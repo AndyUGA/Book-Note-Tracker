315a90769f2e66c2ef3729f4743ba0877b455f94
Andy Truong <att35663@uga.edu>
2019-03-30 20:01:21 -0400

    Connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//DB config connection string
+
+const db = require("./config/keys").MongoURI;
+
+//Connect to mongoose
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 
 app.use(expressLayouts);
 app.set("view engine", "ejs");
