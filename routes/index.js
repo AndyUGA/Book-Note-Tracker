7831f3d0e136dd43b2644c2c1d3ef2d87fd89ed5
Andy <att35663@uga.edu>
2019-04-02 19:41:31 -0400

    Added passport authentication

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+//Welcome
+router.get("/", (req, res) => res.render("welcome"));
+
+//Dashboard
 router.get("/", (req, res) => res.render("welcome"));
 
 module.exports = router;
