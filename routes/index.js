6f7ba313d37cf42fd9f510577b53bdeb03e7bbc8
Andy <att35663@uga.edu>
2019-04-18 12:48:17 -0400

    Display books for their specific account

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var ObjectID = require("mongodb").ObjectID;
 const MongoClient = require("mongodb").MongoClient;
 const uri = require("../config/keys").MongoURI;
 const client = new MongoClient(uri, { useNewUrlParser: true });
@@ -12,16 +13,30 @@ router.get("/", (req, res) => res.render("welcome"));
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
   client.connect(err => {
     const collection = client.db("test").collection("users");
+    let result;
     collection.find({}).toArray(function(err, result) {
       if (err) {
         res.send({ error: " An error has occurred" });
       } else {
-        console.log(req);
+        console.log("Result length is " + result.length);
+
+        const currentID = { _id: ObjectID(req.user._id) };
+        console.log("24");
+        console.log(currentID._id);
+        for (let i = 0; i < result.length; i++) {
+          let dbID = { _id: ObjectID(result[i]._id) };
+          console.log("Comparing " + currentID._id + " with " + dbID._id);
+          if (currentID._id.equals(dbID._id)) {
+            console.log("Name is " + req.user.name);
+            result = result[i];
+          }
+        }
+
         res.render("dashboard", {
           name: req.user.name,
           email: req.user.email,
           id: req.user._id,
-          result: result[0]
+          result: result
         });
       }
     });
