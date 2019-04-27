d20cacf378eca6a273c1ff1b523b8412910b076d
Andy <att35663@uga.edu>
2019-04-27 17:32:46 -0400

    Added nav bar

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,40 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
         console.log(currentID._id);
         for (let i = 0; i < result.length; i++) {
           let dbID = { _id: ObjectID(result[i]._id) };
+          //console.log("Comparing " + currentID._id + " with " + dbID._id);
+          if (currentID._id.equals(dbID._id)) {
+            //console.log("Name is " + req.user.name);
+            result = result[i];
+          }
+        }
+
+        res.render("dashboard", {
+          name: req.user.name,
+          email: req.user.email,
+          id: req.user._id,
+          result: result
+        });
+      }
+    });
+  });
+});
+
+/*
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
+  client.connect(err => {
+    const collection = client.db("test").collection("users");
+    let result;
+    collection.find({}).toArray(function(err, result) {
+      if (err) {
+        res.send({ error: " An error has occurred" });
+      } else {
+        console.log("Result length is " + result.length);
+
+        const currentID = { _id: ObjectID(req.user._id) };
+        console.log("24");
+        console.log(currentID._id);
+        for (let i = 0; i < result.length; i++) {
+          let dbID = { _id: ObjectID(result[i]._id) };
           console.log("Comparing " + currentID._id + " with " + dbID._id);
           if (currentID._id.equals(dbID._id)) {
             console.log("Name is " + req.user.name);
@@ -42,6 +76,7 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
     });
   });
 });
+*/
 
 router.post("/createBookEntry/:name", (req, res, next) => {
   const name = req.params.name;
