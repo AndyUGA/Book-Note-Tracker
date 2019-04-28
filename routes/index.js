822961a724986a4d0ac8c17765e5ca059984bceb
Andy Truong <att35663@uga.edu>
2019-04-28 19:21:08 -0400

    Reformatted how book title and notes are stored

--- routes/index.js
+++ routes/index.js
@@ -18,14 +18,19 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
       if (err) {
         res.send({ error: " An error has occurred" });
       } else {
-        const currentID = { _id: ObjectID(req.user._id) };
+        //console.log("Result length is " + result.length);
 
+        const currentID = { _id: ObjectID(req.user._id) };
+        //console.log("24");
+        //console.log(currentID._id);
         for (let i = 0; i < result.length; i++) {
           let dbID = { _id: ObjectID(result[i]._id) };
           //console.log("Comparing " + currentID._id + " with " + dbID._id);
           if (currentID._id.equals(dbID._id)) {
-            //console.log("Name is " + req.user.name);
+            //console.log("Name is " + req.user.name
             result = result[i];
+            console.log("Result is");
+            console.log(result);
           }
         }
 
@@ -39,49 +44,18 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
     });
   });
 });
-
-/*
-router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  client.connect(err => {
-    const collection = client.db("test").collection("users");
-    let result;
-    collection.find({}).toArray(function(err, result) {
-      if (err) {
-        res.send({ error: " An error has occurred" });
-      } else {
-        console.log("Result length is " + result.length);
-
-        const currentID = { _id: ObjectID(req.user._id) };
-        console.log("24");
-        console.log(currentID._id);
-        for (let i = 0; i < result.length; i++) {
-          let dbID = { _id: ObjectID(result[i]._id) };
-          console.log("Comparing " + currentID._id + " with " + dbID._id);
-          if (currentID._id.equals(dbID._id)) {
-            console.log("Name is " + req.user.name);
-            result = result[i];
-          }
-        }
-
-        res.render("dashboard", {
-          name: req.user.name,
-          email: req.user.email,
-          id: req.user._id,
-          result: result
-        });
-      }
-    });
-  });
-});
-*/
 
 router.post("/createBookEntry/:name", (req, res, next) => {
   const name = req.params.name;
+
   const title = req.body.title;
   let tempData = { Title2: title };
   client.connect(err => {
     const collection = client.db("test").collection("users");
-    collection.updateOne({ name: name }, { $push: { Books: title } });
+    collection.updateOne(
+      { name: name },
+      { $push: { BookTitle: { Title: title, Note: ["Note2"] } } }
+    );
 
     res.redirect("/dashboard");
   });
