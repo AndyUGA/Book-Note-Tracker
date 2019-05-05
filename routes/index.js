bbb81a564f5fb917bf96291bc5387a2f4d5b45c2
Andy Truong <att35663@uga.edu>
2019-05-05 11:45:21 -0400

    Simplified routes

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,13 @@ const { ensureAuthenticated } = require("../config/auth");
 var result;
 
 //Get Welcome page
-router.get("/", (req, res) => res.render("User/Homepage", { layout: "User/Homepage" }));
+router.get("/", (req, res) =>
+  res.render("User/Homepage", { layout: "User/Homepage" })
+);
 
-//Get dashboard page of user
-router.get("/dashboard", ensureAuthenticated, (req, res) => {
+router.get("/:content", ensureAuthenticated, (req, res) => {
+  const content = req.params.content;
+
   client.connect(err => {
     const collection = client.db("test").collection("users");
 
@@ -34,51 +37,27 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
         let fullname;
         firstChar = name.charAt(0);
         fullname = firstChar.toUpperCase() + name.substring(1, name.length);
-        res.render("User/dashboard", {
-          name: name,
-          fullname: fullname,
-          email: req.user.email,
-          id: req.user._id,
-          result: result,
-          title: "Dashboard"
-        });
-      }
-    });
-  });
-});
 
-//Get dashboard page of user
-router.get("/profile", ensureAuthenticated, (req, res) => {
-  client.connect(err => {
-    const collection = client.db("test").collection("users");
-
-    collection.find({}).toArray(function(err, result) {
-      if (err) {
-        res.send({ error: " An error has occurred" });
-      } else {
-        const currentID = { _id: ObjectID(req.user._id) };
-
-        for (let i = 0; i < result.length; i++) {
-          let dbID = { _id: ObjectID(result[i]._id) };
-
-          if (currentID._id.equals(dbID._id)) {
-            result = result[i];
-          }
+        if (content == "dashboard") {
+          res.render("User/dashboard", {
+            name: name,
+            fullname: fullname,
+            email: req.user.email,
+            id: req.user._id,
+            result: result,
+            title: "Dashboard"
+          });
+        } else if (content == "profile") {
+          res.render("User/profile", {
+            layout: "User/profile",
+            name: name,
+            fullname: fullname,
+            email: req.user.email,
+            id: req.user._id,
+            result: result,
+            title: "Profile"
+          });
         }
-
-        let name = req.user.name;
-        let fullname;
-        firstChar = name.charAt(0);
-        fullname = firstChar.toUpperCase() + name.substring(1, name.length);
-        res.render("User/profile", {
-          layout: "User/profile",
-          name: name,
-          fullname: fullname,
-          email: req.user.email,
-          id: req.user._id,
-          result: result,
-          title: "Profile"
-        });
       }
     });
   });
@@ -124,56 +103,80 @@ router.post("/createBookEntry/:name", ensureAuthenticated, (req, res, next) => {
 
   client.connect(err => {
     const collection = client.db("test").collection("users");
-    collection.updateOne({ name: name }, { $push: { BookTitle: { Title: title, Note: [] } } });
+    collection.updateOne(
+      { name: name },
+      { $push: { BookTitle: { Title: title, Note: [] } } }
+    );
 
     res.redirect("/dashboard");
   });
 });
 
 //Requst to create note
-router.post("/insertNote/:index/:name/:bookTitle", ensureAuthenticated, (req, res, next) => {
-  const name = req.params.name;
-  const index = req.params.index;
-  const title = req.body.title;
-  const bookTitle = req.params.bookTitle;
-  const note = req.body.note;
+router.post(
+  "/insertNote/:index/:name/:bookTitle",
+  ensureAuthenticated,
+  (req, res, next) => {
+    const name = req.params.name;
+    const index = req.params.index;
+    const title = req.body.title;
+    const bookTitle = req.params.bookTitle;
+    const note = req.body.note;
 
-  client.connect(err => {
-    const collection = client.db("test").collection("users");
-    collection.updateOne({ name: name, "BookTitle.Title": bookTitle }, { $push: { "BookTitle.$.Note": note } });
+    client.connect(err => {
+      const collection = client.db("test").collection("users");
+      collection.updateOne(
+        { name: name, "BookTitle.Title": bookTitle },
+        { $push: { "BookTitle.$.Note": note } }
+      );
 
-    res.redirect("/getBookNotes/" + index + "/" + name);
-  });
-});
+      res.redirect("/getBookNotes/" + index + "/" + name);
+    });
+  }
+);
 
 //Request to update note
-router.post("/updateNote/:noteIndex/:name/:bookTitle/:bookIndex", ensureAuthenticated, (req, res, next) => {
-  const name = req.params.name;
-  const bookIndex = req.params.bookIndex;
-  const noteIndex = req.params.noteIndex;
-  const title = req.body.title;
-  const bookTitle = req.params.bookTitle;
-  const note = req.body.note;
+router.post(
+  "/updateNote/:noteIndex/:name/:bookTitle/:bookIndex",
+  ensureAuthenticated,
+  (req, res, next) => {
+    const name = req.params.name;
+    const bookIndex = req.params.bookIndex;
+    const noteIndex = req.params.noteIndex;
+    const title = req.body.title;
+    const bookTitle = req.params.bookTitle;
+    const note = req.body.note;
 
-  client.connect(err => {
-    const collection = client.db("test").collection("users");
-    collection.updateOne({ name: name, "BookTitle.Title": bookTitle }, { $set: { ["BookTitle.$.Note." + noteIndex]: note } });
+    client.connect(err => {
+      const collection = client.db("test").collection("users");
+      collection.updateOne(
+        { name: name, "BookTitle.Title": bookTitle },
+        { $set: { ["BookTitle.$.Note." + noteIndex]: note } }
+      );
 
-    res.redirect("/getBookNotes/" + bookIndex + "/" + name);
-  });
-});
+      res.redirect("/getBookNotes/" + bookIndex + "/" + name);
+    });
+  }
+);
 
 //Request to delete book entry
-router.post("/deleteNote/:bookTitle/:name", ensureAuthenticated, (req, res, next) => {
-  const name = req.params.name;
-  const bookTitle = req.params.bookTitle;
+router.post(
+  "/deleteNote/:bookTitle/:name",
+  ensureAuthenticated,
+  (req, res, next) => {
+    const name = req.params.name;
+    const bookTitle = req.params.bookTitle;
 
-  client.connect(err => {
-    const collection = client.db("test").collection("users");
-    collection.updateOne({ name: name, "BookTitle.Title": bookTitle }, { $pull: { BookTitle: { Title: bookTitle } } });
+    client.connect(err => {
+      const collection = client.db("test").collection("users");
+      collection.updateOne(
+        { name: name, "BookTitle.Title": bookTitle },
+        { $pull: { BookTitle: { Title: bookTitle } } }
+      );
 
-    res.redirect("/dashboard");
-  });
-});
+      res.redirect("/dashboard");
+    });
+  }
+);
 
 module.exports = router;
