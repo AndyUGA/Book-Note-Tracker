1c1008d67dce3ba7d03b864fecafe5f6d3737d35
Andy <att35663@uga.edu>
2020-11-12 15:51:25 -0500

    Updated port variable name to be compatible with Heroku

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.use(function(req, res, next) {
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
